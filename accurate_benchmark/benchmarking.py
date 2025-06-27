c3a3a651d8d805224f95b436d4632fa2d977465a
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-06-27 12:44:41 -0700

    Update version to 1.1.0, remove psutil dependency, and refactor Benchmark class methods

--- accurate_benchmark/benchmarking.py
+++ accurate_benchmark/benchmarking.py
@@ -1,11 +1,10 @@
 from collections import deque
 from collections.abc import Callable, Iterable
-from functools import wraps, lru_cache
+from functools import wraps
 from time import perf_counter
 from typing import ParamSpec, TypeVar
 from itertools import repeat
 from scipy.stats import trim_mean
-import psutil
 
 
 P = ParamSpec("P")
@@ -17,8 +16,6 @@ class Benchmark:
     A class to benchmark a function by running it multiple times and printing the average time taken.
     """
 
-    __virtual_memory = psutil.virtual_memory()
-
     def __init__(self, func: Callable[P, R], precision: int = 15) -> None:
         """
         :param func: The function to benchmark.
@@ -28,38 +25,36 @@ class Benchmark:
         """
         self.__func: Callable = func
         self.__precision: int = precision
+        self.__result: float = ...
+        self.__doc__: str | None = self.__func.__doc__
+        self.__name__: str = self.__func.__name__
 
-    def benchmark(
-        self,
-    ) -> Callable[[Callable[P, R]], Callable[P, R]]:
-        def decorator() -> Callable[P, R]:
-            @wraps(self.__func)
-            @lru_cache(self.__virtual_memory.available // 8)
-            def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-                results: deque[float] = deque(maxlen=self.__precision)
-                parameters: tuple[object] = args + tuple(kwargs.items())
-                for _ in repeat(None, self.__precision):
-                    start_time = perf_counter()
-                    self.__func(*args, **kwargs)
-                    end_time = perf_counter()
-                    results.append(end_time - start_time)
-                result: float = trim_mean(results, 0.05)
-                print(f"{self.__func.__name__}{parameters} took {result:.18f} seconds")
+    def benchmark(self, *args: P.args, **kwargs: P.kwargs) -> float:
+        @wraps(self.__func)
+        def wrapper() -> None:
+            results: deque[float] = deque(maxlen=self.__precision)
+            parameters: tuple[object] = args + tuple(kwargs.items())
+            for _ in repeat(None, self.__precision):
+                start_time = perf_counter()
+                self.__func(*args, **kwargs)
+                end_time = perf_counter()
+                results.append(end_time - start_time)
+            self.__result = trim_mean(results, 0.05)
+            print(
+                f"{self.__func.__name__}{parameters} took {self.__result:.18f} seconds"
+            )
 
-                return result
-
-            return wrapper
-
-        return decorator
+        wrapper()
+        return self.__result
 
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
-        return self.benchmark()(self.__func)(*args, **kwargs)
+        return self.__func(*args, **kwargs)
 
     def compare(
         self,
-        func2: Callable[..., R],
-        args1: tuple,
-        args2: tuple,
+        func2: Callable[P, R],
+        args1: tuple | None = None,
+        args2: tuple | None = None,
         accuracy: int = 15,
         kwargs1: dict = ...,
         kwargs2: dict = ...,
@@ -75,23 +70,26 @@ class Benchmark:
         :param accuracy: How many times to run each function, a higher is more accurate than a smaller number but it takes longer
         :returntype None:
         """
+        if args1 is None:
+            args1 = ()
+        if args2 is None:
+            args2 = ()
         if kwargs1 == ...:
             kwargs1 = {}
         if kwargs2 == ...:
             kwargs2 = {}
         precision: int = self.__precision
         self.__precision = accuracy
-        benchmark1 = self
-        benchmark2 = Benchmark(func2, accuracy)
+        benchmark = Benchmark(func2, accuracy)
         if not isinstance(args1, Iterable):
-            time1 = benchmark1(*[args1], **kwargs1)
+            time1 = self.benchmark(*[args1], **kwargs1)
         else:
-            time1 = benchmark1(*args1, **kwargs1)
+            time1 = self.benchmark(*args1, **kwargs1)
         if not isinstance(args2, Iterable):
-            time2 = benchmark2(*[args2], **kwargs2)
+            time2 = benchmark.benchmark(*[args2], **kwargs2)
         else:
-            time2 = benchmark2(*args2, **kwargs2)
+            time2 = benchmark.benchmark(*args2, **kwargs2)
         self.__precision = precision
         print(
-            f"{self.__func.__name__} is {time2 / time1:.4f} times {'faster' if time1 < time2 else 'slower'} than {func2.__name__}"
+            f"{self.__func.__name__} is {time2 / time1 if time1 < time2 else time1 / time2:4f} times {'faster' if time1 < time2 else 'slower' if time2 < time1 else 'the same'} than {func2.__name__}"
         )
