77ded3aab7bf46f356fd8f146b6fa7c8dd4dd8eb
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-07-11 10:04:20 -0700

    Update version in __init__.py and pyproject.toml to 1.4.3

--- accurate_benchmark/__init__.py
+++ accurate_benchmark/__init__.py
@@ -10,4 +10,4 @@ from accurate_benchmark.parameters import SingleParam
 
 
 __all__: list[str] = ["benchmarking", "parameters", "Benchmark", "SingleParam"]
-__version__: str = "1.4.4"
+__version__: str = "1.4.3"
